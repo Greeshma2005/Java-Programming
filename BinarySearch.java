1a6799b1a22a5639cea3013084aa6f1998d502d0
Greeshma2005 <greeshmasree02@gmail.com>
2026-02-10 21:46:32 +0530

    searching

---
+++ BinarySearch.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter element to search:");
+        int key = sc.nextInt();
+
+        int low = 0, high = n - 1;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                index = mid;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Not Found");
+        }
+
+        sc.close();
+    }
+}
